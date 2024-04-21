178bb2df3e9610d237a1a2bcf117dcb9ec867819
nvdvlp <137436428+nvdvlp@users.noreply.github.com>
2024-04-20 21:05:32 -0500

    button fixed whit angular integration

---
+++ irregular-ice/src/components/splash/splash.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-splash',
+    standalone:true,
+    template:``,
+    templateUrl: './splash.component.html',
+    styleUrl:'./splash.comoponent.css'
+})
+
+export class SplashComponent{
+    counter = 0;
+}
