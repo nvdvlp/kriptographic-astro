88a6aa954640500af2531d0a31beb24306b9f732
nvdvlp <137436428+nvdvlp@users.noreply.github.com>
2024-04-22 17:07:36 -0500

    angular cambios

---
+++ irregular-ice/src/components/hamburgerMenu/hamburgerMenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-hamburgerMenu',
+    standalone:true,
+    template:``,
+    templateUrl: './hamburgerMenu.component.html',
+    styleUrl:'./hamburgerMenu.component.css'
+})
+
+export class HamburgerMenu{
+}
--- irregular-ice/src/components/splash/splash.component.ts
+++ irregular-ice/src/components/splash/splash.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { HamburgerMenu } from '../hamburgerMenu/hamburgerMenu.component';
 
 @Component({
     selector:'app-splash',
+    imports: [HamburgerMenu],
     standalone:true,
     template:``,
     templateUrl: './splash.component.html',
-    styleUrl:'./splash.comoponent.css'
+    styleUrl:'./splash.comoponent.css',
 })
 
 export class SplashComponent{
-    counter = 0;
+    hidden: boolean = true;
+
+    open(){
+        console.log("abrir")
+        this.hidden = false;
+    }
 }
