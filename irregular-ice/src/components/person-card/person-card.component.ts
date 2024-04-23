bd925cd1f77e898cf9758f427f170230f0357e92
David Barón <baronavilandavid@gmail.com>
2024-04-22 23:20:15 -0500

    setup inicial de seccion teams

---
+++ irregular-ice/src/components/person-card/person-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-team',
+    standalone:true,
+    template:``,
+    templateUrl: './person-card.component.html',
+    styleUrl:'./person-card.component.css'
+})
+
+export class PersonCardComponent{
+}
---
+++ irregular-ice/src/components/team/team.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-team',
+    standalone:true,
+    template:``,
+    templateUrl: './team.component.html',
+    styleUrl:'./team.component.css'
+})
+
+export class TeamComponent{
+}
