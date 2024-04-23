1bf8186cc55e63c56cc493978d5c83393c3c92d7
David Barón <baronavilandavid@gmail.com>
2024-04-23 13:41:11 -0500

    Se anima el hamburguer menu

--- irregular-ice/src/components/splash/splash.component.ts
+++ irregular-ice/src/components/splash/splash.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HamburgerMenu } from '../hamburgerMenu/hamburgerMenu.component';
 import { NgClass, NgIf } from '@angular/common';
+import { timeout } from 'rxjs';
 
 @Component({
     selector:'app-splash',
@@ -13,9 +14,13 @@ import { NgClass, NgIf } from '@angular/common';
 
 export class SplashComponent{
     showMenu: boolean = false;
+    slide: boolean = false;
 
     open(){
         console.log("abrir")
         this.showMenu = !this.showMenu;
+        setTimeout(() => {
+            this.slide = !this.slide
+        },300)
     }
 }
