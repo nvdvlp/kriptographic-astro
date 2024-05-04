ad7020262e9493bbffd0fdfbcb752f47a259b0b3
nvdvlp <137436428+nvdvlp@users.noreply.github.com>
2024-05-04 10:24:24 -0500

    problema con el componente de about us creado

---
+++ irregular-ice/src/components/aboutUs/aboutUs.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+
+@Component({
+    selector:'app-aboutUs',
+    imports: [ NgClass, NgIf],
+    standalone:true,
+    template:``,
+    templateUrl: './aboutUs.component.html',
+    styleUrl:'./aboutUs.component.css',
+})
+
+export class AboutUsComponent{
+
+}
