36cdf95750d8803bfa6e4295befa691775b4f6f8
nvdvlp <137436428+nvdvlp@users.noreply.github.com>
2024-05-04 16:17:46 -0500

    terminadas todas las secciones iniciales

---
+++ irregular-ice/src/components/joinUs/joinUs.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+
+@Component({
+    selector:'app-joinUs',
+    imports: [ NgClass, NgIf],
+    standalone:true,
+    template:``,
+    templateUrl: './joinUs.component.html',
+    styleUrl:'./joinUs.component.css',
+})
+
+export class JoinUsComponent{
+
+}
